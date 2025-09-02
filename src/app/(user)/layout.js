2da66b4f6c9dfd44795102b3079711fc08735e2e
Mahin-GitHub <mmahin2017@gmail.com>
2025-09-02 19:37:33 +0600

    overflow update

--- src/app/(user)/layout.js
+++ src/app/(user)/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import Navbar from "@/Components/Navbar/Navbar";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -21,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased mx-auto  `}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased mx-auto overflow-hidden  `}
       >
 
 
