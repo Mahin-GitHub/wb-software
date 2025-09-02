68f2ef23271896d5fe6a1052d2f442e842a503de
Mahin-GitHub <mmahin2017@gmail.com>
2025-09-02 17:24:43 +0600

    Updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -44,40 +44,44 @@ const Navbar = ({ children }) => {
 
     return (
 
-        <div className="flex justify-center items-center bg-white sm:my-[50px]">
-            {/* Phone Mockup */}
-            <div className="relative w-full h-screen sm:max-w-[375px] sm:h-[665px] sm:border-6 sm:shadow-2xl sm:border-black sm:rounded-2xl bg-gray-100 flex flex-col">
 
-                {/* Scrollable Screen */}
-                <div className="flex-1 overflow-y-auto no-scrollbar px-4 pt-6 pb-6">
-                    {/* Top Logo */}
-                    <div className="w-full flex justify-center mb-4">
-                        <div className="w-[220px]">
-                            <Image
-                                className="w-full h-auto"
-                                src="/assets/logo.png"
-                                width={1000}
-                                height={1000}
-                                alt="logo"
-                            />
-                        </div>
-                    </div>
 
-                    {/* Content */}
-                    <div className="flex justify-center items-center h-full">
-                        {children}
-                    </div>
-                </div>
+<div className="flex justify-center items-center bg-white sm:my-[50px]">
+  {/* Phone Mockup */}
+  <div className="relative w-full h-screen sm:max-w-[375px] sm:h-[665px] 
+                  sm:border-6 sm:shadow-2xl sm:border-black sm:rounded-2xl 
+                  bg-gray-100 flex flex-col">
 
-                {/*Bottom Menu*/}
-                <div className="bg-white py-2 sm:rounded-b-2xl flex justify-between items-center px-4">
-                    {menuItems.map((item, index) => (
-                        <BottomMenu key={index} item={item} />
-                    ))}
-                </div>
-            </div>
+    {/* Scrollable Screen */}
+    <div className="flex-1 min-h-0 overflow-y-auto no-scrollbar px-4 pt-6 pb-6 flex flex-col">
+      
+      {/* Top Logo (always top) */}
+      <div className="w-full flex justify-center mb-4">
+        <div className="w-[220px]">
+          <Image
+            className="w-full h-auto"
+            src="/assets/logo.png"
+            width={1000}
+            height={1000}
+            alt="logo"
+          />
         </div>
+      </div>
 
+      {/* Children (centered only) */}
+      <div className="flex-1 flex justify-center items-center">
+        {children}
+      </div>
+    </div>
+
+    {/* Bottom Menu */}
+    <div className="bg-white py-2 sm:rounded-b-2xl flex justify-between items-center px-4">
+      {menuItems.map((item, index) => (
+        <BottomMenu key={index} item={item} />
+      ))}
+    </div>
+  </div>
+</div>
 
 
     )
