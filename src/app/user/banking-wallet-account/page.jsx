a00978847733fbddfd41ec839f248220832734b6
Mahin-GitHub <mmahin2017@gmail.com>
2025-09-03 18:27:54 +0600

    update

--- src/app/user/banking-wallet-account/page.jsx
+++ src/app/user/banking-wallet-account/page.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 
 const BankingWaletAccount = () => {
     return (
-        <div  className='w-full h-auto mx-auto sm:mb-12 px-2  py-4 sm:my-8 border-1 border-gray-300 shadow-3xl bg-white'>
-            BankingWaletAccount
+        <div className='w-full h-auto mx-auto sm:mb-12 px-2  py-4 sm:my-8 border-1 border-gray-300 shadow-3xl bg-white'>
+            <div className='flex justify-center items-center w-full mb-2'>
+
+                <h2 className='w-full bg-orange-400 p-2 text-white text-center text-lg font-bold rounded-sm'>Sorry, Account Not Found.!</h2>
             </div>
+
+            <p className='text-center text-lg'>No available account. To create an account click the link below.</p>
+            <div className='flex justify-center items-center w-full mb-2'>
+
+                <button className='w-full bg-orange-400 p-2 text-white font-semibold rounded-sm'>Transaction</button>
+            </div>
+        </div>
     )
 }
 
--- src/app/user/profile/page.jsx
+++ src/app/user/profile/page.jsx
@@ -1,10 +1,32 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Profile = () => {
     return (
-        <div className='w-full h-auto mx-auto sm:mb-12 px-2  py-4 sm:my-8 border-1 border-gray-300 shadow-3xl bg-white'>
-            Profile
-        </div>
+        <>
+            <div className='w-full h-auto mx-auto sm:mb-12 px-2  py-4 sm:my-8 border-1 border-gray-300 shadow-3xl bg-white'>
+
+                <div className='flex justify-start items-center gap-4'>
+                    <div className='w-[130px] h-[130px] border'>
+                        <Image className='w-full h-full'
+                            src={"/assets/profile.jpg"}
+                            width={500}
+                            height={500}
+                            alt='Profile Pic'>
+                        </Image>
+                    </div>
+                    <div>
+                        <h2 className="font-bold text-lg">MD. MIRAJ HOSSEN MAHIN</h2>
+                        <p className='text-sm'>01761823647</p>
+                        <p className='text-sm'>Male</p>
+                        <p className='text-sm'>Software Engineer</p>
+                    </div>
+
+
+                </div>
+            </div >
+        </>
+
     )
 }
 
