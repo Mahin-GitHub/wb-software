72a35976ea1c9d0b6e69a822a18a74ca47c33bcb
Mahin-GitHub <mmahin2017@gmail.com>
2025-09-09 14:45:26 +0600

    mfs-card

--- src/Components/Skeleton/WalletAccountSkeleton.jsx
+++ src/Components/Skeleton/WalletAccountSkeleton.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const WalletAccountSkeleton = () => {
     return (
         <div>
-            <div className="w-full h-auto mx-auto sm:mb-12 px-2 py-4 sm:my-8 border border-gray-300 shadow-lg bg-white animate-pulse">
+            <div className="w-full h-auto mx-autopy-4 p-2 border border-gray-300 shadow-sm bg-white animate-pulse">
 
                 {/* Title Placeholder */}
                 <div className="flex justify-center items-center w-full mb-2">
-                    <div className="w-full h-8 bg-orange-300 rounded-sm"></div>
+                    <div className="w-full h-8 bg-gray-300 rounded-sm"></div>
                 </div>
 
                 {/* Text Placeholder */}
@@ -17,7 +17,7 @@ const WalletAccountSkeleton = () => {
 
                 {/* Button Placeholder */}
                 <div className="flex justify-center items-center w-full">
-                    <div className="w-full h-10 bg-orange-300 rounded-sm"></div>
+                    <div className="w-full h-10 bg-gray-300 rounded-sm"></div>
                 </div>
 
             </div>
